c99f88c50cb9e1b7d231fa78f91970008a8afe2c
Aluno <Aluno@Digital.net>
2018-05-30 20:36:53 -0300

    pagina em aspx de temperaturas ideais

---
+++ teste/java.js
@@ -0,0 +1,11 @@
+function getViewConfiguracoes(){// função para abrir a tela de configuração, acho q voce ja entendeu como funciona isso, se voce viu os exemplos acima, mas vamos lá
+    var div_fundo = document.getElementById("fundo_modal");// adiciona a uma variavel, a referencia que busca a div que possui o ID "fundo modal"
+    div_fundo.className = "";// tira todas as classes da div que referenciamos acima
+    div_fundo.className = "fundo_modal_on"; //adiciona uma classe para que o css aplique o efeito, no caso para mostrar a tela
+}
+
+function removerViewConfiguracoes(){// função para remover a tela de configurações
+    var div_fundo = document.getElementById("fundo_modal");// msm coisa
+    div_fundo.className = "";//remove as classe
+    div_fundo.className = "fundo_modal_off"; //adiciona a classe para esconder a tela, classe adicionada para o css aplicar o efeito
+}
