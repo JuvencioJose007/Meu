a84ff900e31d1f81736d98b5eed4d32bd5b43136
Aluno <Aluno@Digital.net>
2018-06-05 19:00:14 -0300

    atualização exemplo terminar

---
+++ For Csharp/For Csharp/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+
+            //int total = int.Parse(Console.ReadLine());
+            //int[] v = new int[total];
+
+            //int contador = 0;
+            //while (contador < total)
+            //{
+            //    v[contador] = int.Parse(Console.ReadLine());
+            //    contador = contador + 1;
+            //}
+
+            //contador = 0;
+            //while (contador < total)
+            //{
+            //    int dentro = contador + 1;
+            //    while (dentro < total)
+            //    {
+
+            //        if (v[contador] > v[dentro])
+            //        {
+            //            int tmp;
+            //            tmp = v[contador];
+            //            v[contador] = v[dentro];
+            //            v[dentro] = tmp;
+            //        }
+
+            //        dentro = dentro + 1;
+            //    }
+            //    contador = contador + 1;
+            //}
+
+            #endregion
+
+            #region
+
+            //int total = int.Parse(Console.ReadLine());
+            //int[] pontos = new int[total];
+            //string[] nomes = new string[total];
+
+            //int contador = 0;
+            //while (contador < total)
+            //{
+            //    nomes[contador] = Console.ReadLine();
+            //    pontos[contador] = int.Parse(Console.ReadLine());
+            //    contador = contador + 1;
+            //}
+
+            //contador = 0;
+            //while (contador < total)
+            //{
+            //    int dentro = contador + 1;
+            //    while (dentro < total)
+            //    {
+
+            //        if (pontos[contador] > pontos[dentro])
+            //        {
+            //            int tmp;
+            //            tmp = pontos[contador];
+            //            pontos[contador] = pontos[dentro];
+            //            pontos[dentro] = tmp;
+
+            //            string nomeTmp;
+            //            nomeTmp = nomes[contador];
+            //            nomes[contador] = nomes[dentro];
+            //            nomes[dentro] = nomeTmp;
+            //        }
+
+            //        dentro = dentro + 1;
+            //    }
+            //    contador = contador + 1;
+            //}
+
+            #endregion
+
+            #region
+
+            //contador = 0;
+            //while (contador < total)
+            //{
+            //    int dentro = contador + 1;
+            //    while (dentro < total)
+            //    {
+
+            //        if (ano[contador] > ano[dentro])
+            //        {
+            //            //troca
+            //        }
+            //        else if (ano[contador] == ano[dentro])
+            //        {
+            //            if (mes[contador] > mes[dentro])
+            //            {
+            //                //troca
+            //            }
+            //            else if (mes[contador] == mes[dentro])
+            //            {
+            //                if (dia[contador] > dia[dentro])
+            //                {
+            //                    //troca
+            //                }
+            //            }
+            //        }
+
+            //        dentro = dentro + 1;
+            //    }
+            //    contador = contador + 1;
+            //}
+
+            #endregion
+
+        }
+    }
+}
---
+++ exemplo prova 1/exemplo prova 1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo_prova_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int contador = 0;
+            int[] numero = new int[3];
+            string continuar = "sim";
+            while (continuar == "sim") {
+                while (contador < 3)
+                {
+                    Console.Write("Digite um numero: ");
+                    numero[contador] = int.Parse(Console.ReadLine());
+
+                    contador = contador + 1;
+                }
+                Console.WriteLine();
+                Console.WriteLine("Como deseja ordenar os numeros");
+                Console.Write("Digite \"1\" para ordenar de forma crescente ou digite \"-1\" para ordenar de forma decrescente: ");
+                int ordem = int.Parse(Console.ReadLine());
+                if (ordem == 1)
+                {
+                    contador = 0;
+                    while(contador < 10){
+                        int depois = contador + 1;
+                        while (depois < 10)
+                        {
+                            if (numero[contador] > numero[depois])
+                            {
+                                int tmp = numero[contador];
+                                numero[contador] = numero[depois];
+                                numero[depois] = tmp;
+                            }
+                            depois = depois + 1;
+                        }
+                        contador = contador + 1;
+                    }
+                    contador = 0;
+                    while(contador < 10)
+                    {
+                        Console.WriteLine(numero[contador]);
+                        contador++;
+                    }
+                } else if (ordem == -1)
+                {
+                    contador = 0;
+                    while(contador < 10)
+                    {
+                        int depois = contador + 1;
+                        while (depois < 10)
+                        {
+                            if(numero[contador] < numero[depois])
+                            {
+                                int tmp;
+                                tmp = numero[depois];
+                                numero[depois] = numero[contador];
+                                numero[contador] = tmp;
+                            }
+                            depois = depois + 1;
+                        }
+                        contador = contador + 1;
+                    }
+                    contador = 0;
+                    while (contador < 10)
+                    {
+                        Console.WriteLine(numero[contador]);
+                        contador++;
+                    }
+                } else
+                {
+                    Console.WriteLine("Número digitado errado");
+                    Console.Write("Deseja continuar Digite sim: ");
+                    continuar = Console.ReadLine();                    
+                }
+            }
+            Console.Write("Fim programa");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
