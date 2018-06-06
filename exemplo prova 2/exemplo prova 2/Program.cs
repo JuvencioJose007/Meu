c6b45b508769a8c022e74018e8a376f0a2dcfd84
Aluno <Aluno@Digital.net>
2018-06-06 19:09:13 -0300

    exemplos de provas feitas

--- exemplo prova 2/exemplo prova 2/Program.cs
+++ exemplo prova 2/exemplo prova 2/Program.cs
@@ -12,24 +12,49 @@ namespace exemplo_prova_2
         {
             Console.Write("Digite um valor para variaveis: ");
             int total = int.Parse(Console.ReadLine());
-
             int[] val = new int[total];
-            int contador = 1;
-            while (contador <= total)
+            Console.WriteLine();
+            Console.Write("Digite um numero: ");
+            val[0] = int.Parse(Console.ReadLine());
+            
+            int contador = 1;            
+            while (contador < total)
             {
                 int aux = contador - 1;
-                Console.Write("digite um numero: ");
-                val[0] = int.Parse(Console.ReadLine());
-                
+                Console.Write("Digite um numero: ");
+                val[contador] = int.Parse(Console.ReadLine());                
                 while (val[contador] > val[aux])
                 {
-
+                    Console.WriteLine();
+                    Console.WriteLine("Número deve ser digitado menor que o anterior");
+                    Console.Write("Digite um numero: ");
+                    val[contador] = int.Parse(Console.ReadLine());
                 }
                 contador++;
             }
-
-
-
+            int extra;
+            Console.Write("Digite um valor: ");
+            extra = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            contador = 0;
+            while(contador < total)
+            {
+                if (val[contador] % 2 == 0) {
+                        val[contador] = val[contador] + extra;
+                }
+                else {
+                        val[contador] = val[contador] * extra;
+                }
+                contador = contador + 1;
+            }
+            contador = total - 1;
+            while(contador >= 0)
+            {
+                Console.WriteLine(val[contador]);
+                contador--;
+            }
+            
+            Console.ReadKey();
 
         }
     }
